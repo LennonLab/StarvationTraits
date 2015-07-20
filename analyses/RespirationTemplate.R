e29b7920e88b4819a53c481a48024ae17ec70cc5
Mario Muscarella <mmuscare@indiana.edu>
2015-07-20 13:03:29 -0400

    minor change

--- analyses/RespirationTemplate.R
+++ analyses/RespirationTemplate.R
@@ -1,14 +1,15 @@
 ################################################################################
 #                                                                              #
-#  Test Script for Analysis of PreSens Respiration Data                        #
-#   Version 2.0                                                                #
-#  Written By: Mario Muscarella                                                #
+# Respiration Analysis for Starvation Traits Experiment                        #
+#                                                                              #
+#  Written By: Rachel Ferril & Mario Muscarella                                #
 #  Last Update: 28 Jan 2014                                                    #
 #                                                                              #
 #  Use this file to test the PreSens.Respiration fucntion on local machine     #
 #  And as a template to create your own analysis                               #
 #                                                                              #
-#  Template for Respiration data                                               #
+#  Analysis of PreSens Respiration Data                                        #
+#   Version 2.0                                                                #
 #                                                                              #
 ################################################################################
 
@@ -27,15 +28,15 @@ PreSens.Respiration(infile = "./data/Respiration/ExampleData.txt",
                     outfile = "./data/Respiration/ExampleData_Output.txt")
 
 
-# Example txt analysis
+# Respiration Analysis: 7/1/2015
 PreSens.Respiration(infile = "./data/Respiration/20150701_BacterialRespiration_a_RNF_Oxygen.txt",
                     outfile = "./data/Respiration/20150701_BacterialRespiration_a_RNF_Output.txt")
 
-# Example txt analysis
+# Respiration Analysis: 7/1/2015
 PreSens.Respiration(infile = "./data/Respiration/20150701_BacterialRespiration_b_RNF_Oxygen.txt",
                     outfile = "./data/Respiration/20150701_BacterialRespiration_b_RNF_Output.txt")
 
 
-# Example txt analysis
+# Respiration Analysis: 7/1/2015
 PreSens.Respiration(infile = "./data/Respiration/20150701_BacterialRespiration_c_RNF_Oxygen.txt",
                     outfile = "./data/Respiration/20150701_BacterialRespiration_c_RNF_Output.txt")
