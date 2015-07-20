cba10428d8f34ef185f069936131634d5f045ba6
Rachel Ferrill <rnferrill16@transy.edu>
2015-07-20 17:48:05 -0400

    named and graphed remaining respiration samples using Mario's code

--- analyses/RespirationTemplate.R
+++ analyses/RespirationTemplate.R
@@ -39,3 +39,30 @@ PreSens.Respiration(infile = "./data/Respiration/20150701_BacterialRespiration_b
 # Example txt analysis
 PreSens.Respiration(infile = "./data/Respiration/20150701_BacterialRespiration_c_RNF_Oxygen.txt",
                     outfile = "./data/Respiration/20150701_BacterialRespiration_c_RNF_Output.txt")
+
+PreSens.Respiration(infile = "./data/Respiration/20150707_BacterialRespiration_d_RNF_Oxygen.txt",
+                    outfile = "./data/Respiration/20150707_BacterialRespiration_d_RNF_Output.txt")
+
+PreSens.Respiration(infile = "./data/Respiration/20150707_BacterialRespiration_e_RNF_Oxygen.txt",
+                    outfile = "./data/Respiration/20150707_BacterialRespiration_e_RNF_Output.txt")
+
+PreSens.Respiration(infile = "./data/Respiration/20150710_BacterialRespiration_f_RNF_Oxygen.txt",
+                    outfile = "./data/Respiration/20150710_BacterialRespiration_f_RNF_Output.txt")
+
+PreSens.Respiration(infile = "./data/Respiration/20150710_BacterialRespiration_g_RNF_Oxygen.txt",
+                    outfile = "./data/Respiration/20150710_BacterialRespiration_g_RNF_Output.txt")
+
+PreSens.Respiration(infile = "./data/Respiration/20150714_BacterialRespiration_h_RNF_Oxygen.txt",
+                    outfile = "./data/Respiration/20150714_BacterialRespiration_h_RNF_Output.txt")
+
+PreSens.Respiration(infile = "./data/Respiration/20150715_BacterialRespiration_i_RNF_Oxygen.txt",
+                    outfile = "./data/Respiration/20150715_BacterialRespiration_i_RNF_Output.txt")
+
+PreSens.Respiration(infile = "./data/Respiration/20150715_BacterialRespiration_j_RNF_Oxygen.txt",
+                    outfile = "./data/Respiration/20150715_BacterialRespiration_j_RNF_Output.txt")
+
+PreSens.Respiration(infile = "./data/Respiration/20150716_BacterialRespiration_k_RNF_Oxygen.txt",
+                    outfile = "./data/Respiration/20150716_BacterialRespiration_k_RNF_Output.txt")
+
+PreSens.Respiration(infile = "./data/Respiration/20150716_BacterialRespiration_l_RNF_Oxygen.txt",
+                    outfile = "./data/Respiration/20150716_BacterialRespiration_l_RNF_Output.txt")
