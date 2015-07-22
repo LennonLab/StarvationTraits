8055cd831a75225f12d82c54da7fd148e06e2c09
Mario Muscarella <mmuscare@indiana.edu>
2015-07-22 11:26:22 -0400

    Initial commit

---
+++ analyses/tests/PreSensTest.R
@@ -0,0 +1,26 @@
+################################################################################
+#                                                                              #
+#  Test Script for Analysis of PreSens Respiration Data                        #
+#   Version 2.0                                                                #
+#  Written By: Mario Muscarella                                                #
+#  Last Update: 28 Jan 2014                                                    #
+#                                                                              #
+#  Use this file to test the PreSens.Respiration fucntion on local machine     #
+#  And as a template to create your own analysis                               #
+#                                                                              #
+################################################################################
+
+setwd("~/GitHub/StarvationTraits/")
+rm(list=ls())
+
+# Inport the function from source file
+source("./bin/PreSensInteractiveRegression.R")
+
+################################################################################
+# Examples #####################################################################
+################################################################################
+
+# Example txt analysis
+PreSens.Respiration(infile = "./data/Respiration/ExampleData.txt",
+                    outfile = "./data/Respiration/ExampleData_Output.txt")
+
