1aa0af8ac9f4f8620f138edab94d01d9951da2f4
Mario Muscarella <mmuscare@indiana.edu>
2015-07-22 12:09:59 -0400

    moved RespirationGraphs.R, renamed RespirationResponses.R, Edited first plot

---
+++ analyses/RespirationResponses.R
@@ -0,0 +1,358 @@
+###################################################################
+#  Rachel Ferrill
+#  6 Jul 2015
+#  Respiration Graphs
+####################################################################
+
+setwd("~/GitHub/StarvationTraits")
+rm(list=ls())
+
+# Define Functions
+sem <- function(x){sd(x)/sqrt(3)}
+
+# Import Data Files (Output from PreSens Respiration Analysis)
+resp1  <- read.csv("./data/Respiration/20150701_BacterialRespiration_a_RNF_Output.txt")
+resp2  <- read.csv("./data/Respiration/20150701_BacterialRespiration_b_RNF_Output.txt")
+resp3  <- read.csv("./data/Respiration/20150701_BacterialRespiration_c_RNF_Output.txt")
+resp4  <- read.csv("./data/Respiration/20150707_BacterialRespiration_d_RNF_Output.txt")
+resp5  <- read.csv("./data/Respiration/20150707_BacterialRespiration_e_RNF_Output.txt")
+resp6  <- read.csv("./data/Respiration/20150710_BacterialRespiration_f_RNF_Output.txt")
+resp7  <- read.csv("./data/Respiration/20150710_BacterialRespiration_g_RNF_Output.txt")
+resp8  <- read.csv("./data/Respiration/20150714_BacterialRespiration_h_RNF_Output.txt")
+resp9  <- read.csv("./data/Respiration/20150715_BacterialRespiration_i_RNF_Output.txt")
+resp10 <- read.csv("./data/Respiration/20150715_BacterialRespiration_j_RNF_Output.txt")
+resp11 <- read.csv("./data/Respiration/20150716_BacterialRespiration_k_RNF_Output.txt")
+resp12 <- read.csv("./data/Respiration/20150716_BacterialRespiration_l_RNF_Output.txt")
+
+# Seperate Based on Isolate
+# Plate 1
+A7022 <- resp1[1:3,]
+A7022_mean <- mean(A7022$Rate..uM.O2.Hr.1.)
+A7022_sem <- sem(A7022$Rate..uM.O2.Hr.1.)
+B7022 <- resp1[4:6,]
+B7022_mean <- mean(B7022$Rate..uM.O2.Hr.1.)
+B7022_sem <- sem(B7022$Rate..uM.O2.Hr.1.)
+C7022 <- resp1[7:9,]
+C7022_mean <-mean(C7022$Rate..uM.O2.Hr.1.)
+C7022_sem <- sem(C7022$Rate..uM.O2.Hr.1.)
+D7022 <- resp1[10:12,]
+D7022_mean <-mean(D7022$Rate..uM.O2.Hr.1.)
+D7022_sem <-sem(D7022$Rate..uM.O2.Hr.1.)
+A7025 <- resp1[13:15,]
+A7025_mean <-mean(A7025$Rate..uM.O2.Hr.1.)
+A7025_sem <-sem(A7025$Rate..uM.O2.Hr.1.)
+B7025 <- resp1[16:18,]
+B7025_mean <-mean(B7025$Rate..uM.O2.Hr.1.)
+B7025_sem <-sem(B7025$Rate..uM.O2.Hr.1.)
+C7025 <- resp1[19:21,]
+C7025_mean <-mean(C7025$Rate..uM.O2.Hr.1.)
+C7025_sem <-sem(C7025$Rate..uM.O2.Hr.1.)
+mean(C7025[,4])
+sem(C7025[,4])
+
+# Plate 2
+D7025 <- resp2[1:3,]
+D7025_mean <-mean(D7025$Rate..uM.O2.Hr.1.)
+D7025_sem <-sem(D7025$Rate..uM.O2.Hr.1.)
+A7026 <- resp2[4:6,]
+A7026_mean <-mean(A7026$Rate..uM.O2.Hr.1.)
+A7026_sem <-sem(A7026$Rate..uM.O2.Hr.1.)
+B7026 <- resp2[7:9,]
+B7026_mean <-mean(B7026$Rate..uM.O2.Hr.1.)
+B7026_sem <-sem(B7026$Rate..uM.O2.Hr.1.)
+C7026 <- resp2[10:12,]
+C7026_mean <-mean(C7026$Rate..uM.O2.Hr.1.)
+C7026_sem <-sem(C7026$Rate..uM.O2.Hr.1.)
+D7026 <- resp2[13:15,]
+D7026_mean <-mean(D7026$Rate..uM.O2.Hr.1.)
+D7026_sem <-sem(D7026$Rate..uM.O2.Hr.1.)
+A7031 <- resp2[16:18,]
+A7031_mean <-mean(A7031$Rate..uM.O2.Hr.1.)
+A7031_sem <-sem(A7031$Rate..uM.O2.Hr.1.)
+B7031 <- resp2[19:21,]
+B7031_mean <-mean(B7031$Rate..uM.O2.Hr.1.)
+B7031_sem <-sem(B7031$Rate..uM.O2.Hr.1.)
+
+# Plate 3
+A7032 <- resp3[1:3,]
+A7032_mean <-mean(A7032$Rate..uM.O2.Hr.1.)
+A7032_sem <-sem(A7032$Rate..uM.O2.Hr.1.)
+B7032 <- resp3[4:6,]
+B7032_mean <-mean(B7032$Rate..uM.O2.Hr.1.)
+B7032_sem <-sem(B7032$Rate..uM.O2.Hr.1.)
+C7032 <- resp3[7:9,]
+C7032_mean <-mean(C7032$Rate..uM.O2.Hr.1.)
+C7032_sem <-sem(C7032$Rate..uM.O2.Hr.1.)
+D7032 <- resp3[10:12,]
+D7032_mean <-mean(D7032$Rate..uM.O2.Hr.1.)
+D7032_sem <-sem(D7032$Rate..uM.O2.Hr.1.)
+A7034 <- resp3[13:15,]
+A7034_mean <-mean(A7034$Rate..uM.O2.Hr.1.)
+A7034_sem <-sem(A7034$Rate..uM.O2.Hr.1.)
+
+# Plate 4
+B7034 <- resp4[1:3,]
+B7034_mean <- mean(B7034$Rate..uM.O2.Hr.1.)
+B7034_sem <- sem(B7034$Rate..uM.O2.Hr.1.)
+C7034 <- resp4[4:6,]
+C7034_mean <- mean(C7034$Rate..uM.O2.Hr.1.)
+C7034_sem <- sem(C7034$Rate..uM.O2.Hr.1.)
+A7101 <- resp4[7:9,]
+A7101_mean <-mean(A7101$Rate..uM.O2.Hr.1.)
+A7101_sem <-sem(A7101$Rate..uM.O2.Hr.1.)
+B7101 <- resp4[10:12,]
+B7101_mean <-mean(B7101$Rate..uM.O2.Hr.1.)
+B7101_sem <-sem(B7101$Rate..uM.O2.Hr.1.)
+C7101 <- resp4[13:15,]
+C7101_mean <-mean(C7101$Rate..uM.O2.Hr.1.)
+C7101_sem <-sem(C7101$Rate..uM.O2.Hr.1.)
+D7101 <- resp4[16:18,]
+D7101_mean <-mean(D7101$Rate..uM.O2.Hr.1.)
+D7101_sem <-sem(D7101$Rate..uM.O2.Hr.1.)
+
+# Plate 5
+A7102 <- resp5[1:4,]
+A7102_mean <- mean(A7102$Rate..uM.O2.Hr.1.)
+A7102_sem <- sem(A7102$Rate..uM.O2.Hr.1.)
+B7102 <- resp5[4:6,]
+B7102_mean <- mean(B7102$Rate..uM.O2.Hr.1.)
+B7102_sem <- sem(B7102$Rate..uM.O2.Hr.1.)
+C7102 <- resp5[7:9,]
+C7102_mean <-mean(C7102$Rate..uM.O2.Hr.1.)
+C7102_sem <- sem(C7102$Rate..uM.O2.Hr.1.)
+D7102 <- resp5[10:12,]
+D7102_mean <-mean(D7102$Rate..uM.O2.Hr.1.)
+D7102_sem <-sem(D7102$Rate..uM.O2.Hr.1.)
+A7103 <- resp5[13:15,]
+A7103_mean <-mean(A7103$Rate..uM.O2.Hr.1.)
+A7103_sem <-sem(A7103$Rate..uM.O2.Hr.1.)
+B7103 <- resp5[16:18,]
+B7103_mean <-mean(B7103$Rate..uM.O2.Hr.1.)
+B7103_sem <-sem(B7103$Rate..uM.O2.Hr.1.)
+C7103 <- resp5[19:21,]
+C7103_mean <-mean(C7103$Rate..uM.O2.Hr.1.)
+C7103_sem <-sem(C7103$Rate..uM.O2.Hr.1.)
+
+# Plate 6
+D7103 <- resp6[1:3,]
+D7103_mean <-mean(D7103$Rate..uM.O2.Hr.1.)
+D7103_sem <-sem(D7103$Rate..uM.O2.Hr.1.)
+A7231 <- resp6[4:6,]
+A7231_mean <-mean(A7231$Rate..uM.O2.Hr.1.)
+A7231_sem <-sem(A7231$Rate..uM.O2.Hr.1.)
+B7231 <- resp6[7:9,]
+B7231_mean <-mean(B7231$Rate..uM.O2.Hr.1.)
+B7231_sem <-sem(B7231$Rate..uM.O2.Hr.1.)
+C7231 <- resp6[10:12,]
+C7231_mean <-mean(C7231$Rate..uM.O2.Hr.1.)
+C7231_sem <-sem(C7231$Rate..uM.O2.Hr.1.)
+D7231 <- resp6[13:15,]
+D7231_mean <-mean(D7231$Rate..uM.O2.Hr.1.)
+D7231_sem <-sem(D7231$Rate..uM.O2.Hr.1.)
+A7232 <- resp6[16:18,]
+A7232_mean <-mean(A7232$Rate..uM.O2.Hr.1.)
+A7232_sem <-sem(A7232$Rate..uM.O2.Hr.1.)
+B7232 <- resp6[19:21,]
+B7232_mean <-mean(B7232$Rate..uM.O2.Hr.1.)
+B7232_sem <-sem(B7232$Rate..uM.O2.Hr.1.)
+
+# Plate 7
+C7031 <- resp7[1:3,]
+C7031_mean <-mean(C7031$Rate..uM.O2.Hr.1.)
+C7031_sem <-sem(C7031$Rate..uM.O2.Hr.1.)
+D7031 <- resp7[4:6,]
+D7031_mean <-mean(D7031$Rate..uM.O2.Hr.1.)
+D7031_sem <-sem(D7031$Rate..uM.O2.Hr.1.)
+A702 <- resp7[7:9,]
+A702_mean <-mean(A702$Rate..uM.O2.Hr.1.)
+A702_sem <-sem(A702$Rate..uM.O2.Hr.1.)
+A703 <- resp7[10:12,]
+A703_mean <-mean(A703$Rate..uM.O2.Hr.1.)
+A703_sem <-sem(A703$Rate..uM.O2.Hr.1.)
+A710 <- resp7[13:15,]
+A710_mean <-mean(A710$Rate..uM.O2.Hr.1.)
+A710_sem <-sem(A710$Rate..uM.O2.Hr.1.)
+A723 <- resp7[16:18,]
+A723_mean <-mean(A723$Rate..uM.O2.Hr.1.)
+A723_sem <-sem(A723$Rate..uM.O2.Hr.1.)
+
+# Plate 8
+C7232 <- resp8[1:3,]
+C7232_mean <-mean(C7232$Rate..uM.O2.Hr.1.)
+C7232_sem <-sem(C7232$Rate..uM.O2.Hr.1.)
+D7232 <- resp8[4:6,]
+D7232_mean <-mean(D7232$Rate..uM.O2.Hr.1.)
+D7232_sem <-sem(D7232$Rate..uM.O2.Hr.1.)
+A7233 <- resp8[7:9,]
+A7233_mean <-mean(A7233$Rate..uM.O2.Hr.1.)
+A7233_sem <-sem(A7233$Rate..uM.O2.Hr.1.)
+B7233 <- resp8[10:12,]
+B7233_mean <-mean(B7233$Rate..uM.O2.Hr.1.)
+B7233_sem <-sem(B7233$Rate..uM.O2.Hr.1.)
+C7233 <- resp8[13:15,]
+C7233_mean <-mean(C7233$Rate..uM.O2.Hr.1.)
+C7233_sem <-sem(C7233$Rate..uM.O2.Hr.1.)
+D7233 <- resp8[16:18,]
+D7233_mean <-mean(D7233$Rate..uM.O2.Hr.1.)
+D7233_sem <-sem(D7233$Rate..uM.O2.Hr.1.)
+
+# Plate 9
+B7241 <- resp9[1:3,]
+B7241_mean <-mean(B7241$Rate..uM.O2.Hr.1.)
+B7241_sem <-sem(B7241$Rate..uM.O2.Hr.1.)
+C7241 <- resp9[4:6,]
+C7241_mean <-mean(C7241$Rate..uM.O2.Hr.1.)
+C7241_sem <-sem(C7241$Rate..uM.O2.Hr.1.)
+D7241 <- resp9[7:9,]
+D7241_mean <-mean(D7241$Rate..uM.O2.Hr.1.)
+D7241_sem <-sem(D7241$Rate..uM.O2.Hr.1.)
+A7242 <- resp9[10:12,]
+A7242_mean <-mean(A7242$Rate..uM.O2.Hr.1.)
+A7242_sem <-sem(A7242$Rate..uM.O2.Hr.1.)
+B7242 <- resp9[13:15,]
+B7242_mean <-mean(B7242$Rate..uM.O2.Hr.1.)
+B7242_sem <-sem(B7242$Rate..uM.O2.Hr.1.)
+C7242 <- resp9[16:18,]
+C7242_mean <-mean(C7242$Rate..uM.O2.Hr.1.)
+C7242_sem <-sem(C7242$Rate..uM.O2.Hr.1.)
+D7242 <- resp9[19:21,]
+D7242_mean <-mean(D7242$Rate..uM.O2.Hr.1.)
+D7242_sem <-sem(D7242$Rate..uM.O2.Hr.1.)
+
+# Plate 10
+A7243 <- resp10[1:3,]
+A7243_mean <-mean(A7243$Rate..uM.O2.Hr.1.)
+A7243_sem <-sem(A7243$Rate..uM.O2.Hr.1.)
+B7243 <- resp10[4:6,]
+B7243_mean <-mean(B7243$Rate..uM.O2.Hr.1.)
+B7243_sem <-sem(B7243$Rate..uM.O2.Hr.1.)
+C7243 <- resp10[7:9,]
+C7243_mean <-mean(C7243$Rate..uM.O2.Hr.1.)
+C7243_sem <-sem(C7243$Rate..uM.O2.Hr.1.)
+D7243 <- resp10[10:12,]
+D7243_mean <-mean(D7243$Rate..uM.O2.Hr.1.)
+D7243_sem <-sem(D7243$Rate..uM.O2.Hr.1.)
+A724 <- resp10[13:15,]
+A724_mean <-mean(A724$Rate..uM.O2.Hr.1.)
+A724_sem <-sem(A724$Rate..uM.O2.Hr.1.)
+D7034 <- resp10[16:18,]
+D7034_mean <-mean(D7034$Rate..uM.O2.Hr.1.)
+D7034_sem <-sem(D7034$Rate..uM.O2.Hr.1.)
+A7241 <- resp10[19:21,]
+A7241_mean <-mean(A7241$Rate..uM.O2.Hr.1.)
+A7241_sem <-sem(A7241$Rate..uM.O2.Hr.1.)
+
+# Plate 11
+A7011 <- resp11[1:3,]
+A7011_mean <-mean(A7011$Rate..uM.O2.Hr.1.)
+A7011_sem <-sem(A7011$Rate..uM.O2.Hr.1.)
+B7011 <- resp11[4:6,]
+B7011_mean <-mean(B7011$Rate..uM.O2.Hr.1.)
+B7011_sem <-sem(B7011$Rate..uM.O2.Hr.1.)
+C7011 <- resp11[7:9,]
+C7011_mean <-mean(C7011$Rate..uM.O2.Hr.1.)
+C7011_sem <-sem(C7011$Rate..uM.O2.Hr.1.)
+D7011 <- resp11[10:12,]
+D7011_mean <-mean(D7011$Rate..uM.O2.Hr.1.)
+D7011_sem <-sem(D7011$Rate..uM.O2.Hr.1.)
+A7011b <- resp11[13:15,]
+A7011b_mean <-mean(A7011b$Rate..uM.O2.Hr.1.)
+A7011b_sem <-sem(A7011b$Rate..uM.O2.Hr.1.)
+B7011b <- resp11[16:18,]
+B7011b_mean <-mean(B7011b$Rate..uM.O2.Hr.1.)
+B7011b_sem <-sem(B7011b$Rate..uM.O2.Hr.1.)
+C7011b <- resp11[19:21,]
+C7011b_mean <-mean(C7011b$Rate..uM.O2.Hr.1.)
+C7011b_sem <-sem(C7011b$Rate..uM.O2.Hr.1.)
+
+# Plate 12
+D7011b <- resp12[1:3,]
+D7011b_mean <-mean(D7011b$Rate..uM.O2.Hr.1.)
+D7011b_sem <-sem(D7011b$Rate..uM.O2.Hr.1.)
+A7013 <- resp12[4:6,]
+A7013_mean <-mean(A7013$Rate..uM.O2.Hr.1.)
+A7013_sem <-sem(A7013$Rate..uM.O2.Hr.1.)
+B7013 <- resp12[7:9,]
+B7013_mean <-mean(B7013$Rate..uM.O2.Hr.1.)
+B7013_sem <-sem(B7013$Rate..uM.O2.Hr.1.)
+C7013 <- resp12[10:12,]
+C7013_mean <-mean(C7013$Rate..uM.O2.Hr.1.)
+C7013_sem <-sem(C7013$Rate..uM.O2.Hr.1.)
+D7013 <- resp12[13:15,]
+D7013_mean <-mean(D7013$Rate..uM.O2.Hr.1.)
+D7013_sem <-sem(D7013$Rate..uM.O2.Hr.1.)
+A701 <- resp12[16:18,]
+A701_mean <-mean(A701$Rate..uM.O2.Hr.1.)
+A701_sem <-sem(A701$Rate..uM.O2.Hr.1.)
+
+
+# Combine means and sems based on isolate
+means701 <-c(A701_mean, A7011_mean, B7011_mean, C7011_mean, D7011_mean, A7011b_mean, B7011b_mean,
+           C7011b_mean, D7011b_mean, A7013_mean, B7013_mean, C7013_mean, D7013_mean)
+means702 <-c(A702_mean, A7022_mean, B7022_mean, C7022_mean, D7022_mean, A7025_mean, B7025_mean,
+            C7025_mean, D7025_mean, A7026_mean, B7026_mean, C7026_mean, D7026_mean)
+means703 <-c(A703_mean, A7031_mean, B7031_mean, C7031_mean, D7031_mean, A7032_mean, B7032_mean,
+             C7032_mean, D7032_mean, A7034_mean, B7034_mean, C7034_mean, D7034_mean)
+means710 <-c(A710_mean, A7101_mean, B7101_mean, C7101_mean, D7101_mean, A7102_mean, B7102_mean,
+             C7102_mean, D7102_mean, A7103_mean, B7103_mean, C7103_mean, D7103_mean)
+means723 <-c(A723_mean, A7231_mean, B7231_mean, C7231_mean, D7231_mean, A7232_mean, B7232_mean,
+             C7232_mean, D7232_mean, A7233_mean, B7233_mean, C7233_mean, D7233_mean)
+means724 <-c(A724_mean, A7241_mean, B7241_mean, C7241_mean, D7241_mean, A7242_mean, B7242_mean,
+             C7242_mean, D7242_mean, A7243_mean, B7243_mean, C7243_mean, D7243_mean)
+
+sems701 <-c(A701_sem, A7011_sem, B7011_sem, C7011_sem, D7011_sem, A7011b_sem, B7011b_sem,
+           C7011b_sem, D7011b_sem, A7013_sem, B7013_sem, C7013_sem, D7013_sem)
+sems702 <-c(A702_sem, A7022_sem, B7022_sem, C7022_sem, D7022_sem, A7025_sem, B7025_sem, C7025_sem,
+            D7025_sem,A7026_sem, B7026_sem, C7026_sem, D7026_sem)
+sems703 <-c(A703_sem, A7031_sem, B7031_sem, C7031_sem, D7031_sem, A7032_sem, B7032_sem, C7032_sem,
+            D7032_sem, A7034_sem, B7034_sem, C7034_sem, D7034_sem)
+sems710 <-c(A710_sem, A7101_sem, B7101_sem, C7101_sem, D7101_sem, A7102_sem, B7102_sem,
+            C7102_sem, D7102_sem, A7103_sem, B7103_sem, C7103_sem, D7103_sem)
+sems723 <-c(A723_sem, A7231_sem, B7231_sem, C7231_sem, D7231_sem, A7232_sem, B7232_sem,
+            C7232_sem, D7232_sem, A7233_sem, B7233_sem, C7233_sem, D7233_sem)
+sems724 <-c(A724_sem, A7241_sem, B7241_sem, C7241_sem, D7241_sem, A7242_sem, B7242_sem,
+            C7242_sem, D7242_sem, A7243_sem, B7243_sem, C7243_sem, D7243_sem)
+
+# Set Default Plot Parameters
+par(mar=c(6, 5, 1, 1) + 0.1)
+
+# Plot Respiration Responses by Organism
+bp701 <-barplot(means701, las=2, ylim = c(0, 1.2*max(means701+sems701)),
+                ylab=expression(paste("Respiration (",mu,"M O"^2," Hr"^-1,")")),
+                las = 2, names.arg=c("A701", "A7011", "B7011", "C7011", "D7011",
+                "A7011b", "B7011b", "C7011b", "D7011b", "A7013", "B7013",
+                "C7013", "D7013"),
+                col = c("black", rep("gray48", 4), rep("gray73", 4), rep("gray98", 4)))
+mtext("Isolate", side=1, line = 4.5)
+arrows(x0=bp701, y0=means701, y1=means701-sems701, angle=90, length=0.1, lwd=1)
+arrows(x0=bp701, y0=means701, y1=means701+sems701, angle=90, length=0.1, lwd=1)
+arrows(x0=bp701[1], y0=means701[1], y1=means701[1]-sems701[1], angle=90,
+       length=0.1, lwd=1, col = "white")
+legend("topright", legend = c("Ancestor", "Tube 1", "Tube 2", "Tube 3"),
+       fill=c("Black", "gray48", "gray73","gray98"), bty="n")
+
+bp702 <-barplot(means702, ylab=expression(paste("Respiration (", mu, "M O2 Hr-1)")), las = 2, names.arg=c("A702", "A7022", "B7022", "C7022", "D7022", "A7025", "B7025", "C7025", "D7025", "A7026", "B7026", "C7026", "D7026"))
+mtext("Isolate", side=1, line = 4.5)
+arrows(x0=bp702, y0=means702, y1=means702-sems702, angle=90, length=0.1, lwd=1) # length
+arrows(x0=bp702, y0=means702, y1=means702+sems702, angle=90, length=0.1, lwd=1)
+
+bp703 <-barplot(means703, ylab=expression(paste("Respiration (", mu, "M O2 Hr-1)")), las = 2, names.arg=c("A703", "A7031", "B7031", "C7031", "D7031", "A7032", "B7032", "C7032", "D7032", "A7034", "B7034", "C7034", "D7034"))
+mtext("Isolate", side=1, line = 4.5)
+arrows(x0=bp703, y0=means703, y1=means703-sems703, angle=90, length=0.1, lwd=1)
+arrows(x0=bp703, y0=means703, y1=means703+sems703, angle=90, length=0.1, lwd=1)
+
+bp710 <-barplot(means710, ylab=expression(paste("Respiration (", mu, "M O2 Hr-1)")), las = 2, names.arg=c("A710", "A7101", "B7101", "C7101", "D7101", "A7102", "B7102", "C7102", "D7102", "A7103", "B7103", "C7103", "D7103"))
+mtext("Isolate", side=1, line = 4.5)
+arrows(x0=bp710, y0=means710, y1=means710-sems710, angle=90, length=0.1, lwd=1)
+arrows(x0=bp710, y0=means710, y1=means710+sems710, angle=90, length=0.1, lwd=1)
+
+bp723 <-barplot(means723, ylab=expression(paste("Respiration (", mu, "M O2 Hr-1)")), las = 2, names.arg=c("A723", "A7231", "B7231", "C7231", "D7231", "A7232", "B7232", "C7232", "D7232", "A7233", "B7233", "C7233", "D7233"))
+mtext("Isolate", side=1, line = 4.5)
+arrows(x0=bp723, y0=means723, y1=means723-sems723, angle=90, length=0.1, lwd=1)
+arrows(x0=bp723, y0=means723, y1=means723+sems723, angle=90, length=0.1, lwd=1)
+
+bp724 <-barplot(means724, ylab=expression(paste("Respiration (", mu, "M O2 Hr-1)")), las = 2, names.arg=c("A724", "A7241", "B7241", "C7241", "D7241", "A7242", "B7242", "C7242", "D7242", "A7243", "B7243", "C7243", "D7243"))
+mtext("Isolate", side=1, line = 4.5)
+arrows(x0=bp724, y0=means724, y1=means724-sems724, angle=90, length=0.1, lwd=1)
+arrows(x0=bp724, y0=means724, y1=means724+sems724, angle=90, length=0.1, lwd=1)
+
